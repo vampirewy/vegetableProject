b831483dbcd815d3c0c48eb3d94a5f212256ca9b
cad2800 <haveaballmyball@hotmail.com>
2018-03-26 22:33:34 +0800

    创建主页

---
+++ src/components/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <header class="head">
+      <h1>sdfsf</h1>
+      <h2>w42424</h2>
+    </header>
+    <div class="content"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'HomePage'
+  }
+</script>
+
+<style scoped lang="less">
+  .head{
+    h1,h2{
+      padding:0;
+      margin:0;
+    }
+    h1{
+      color:red;
+    }
+    h2{
+      color:blue;
+    }
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HomePage from '@/components/HomePage'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+   //重定向
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path:'*',
+      redirect:'homepage'
+    },
+    {
+      path:'/homepage',
+      name:'首页',
+      component:HomePage
     }
   ]
 })
