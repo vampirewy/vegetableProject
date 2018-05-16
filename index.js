9e82438d9bf6b9adc4ce82957480f89eb03502f2
冰凉的小肉包 <haveaballmyabll@hotmail.com>
2018-05-16 17:22:08 +0800

    关于label:statement语句 与 for循环的区别

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-const a=1;
-alert(a);
+//label:statement 语句
+var num=0;
+start:for (var i = 0 ; i < 8 ; i++){
+  for (var j = 0 ; j < 8 ; j++){
+    if( i == 4 && j == 4 ){
+      break start;
+    }
+    num++;  
+  }
+}
+console.log(num);
+//普通循环语句
+// var num = 0;
+// for (var i = 0 ; i < 10 ; i++){
+//   for (var j = 0 ; j < 10 ; j++){
+//     if( i == 5 && j == 5 ){
+//       break;
+//     }
+//     num++; 
+//   }
+// }
+// console.log(num);
+//普通循环语句
+var num=0,i=0;
+while(i<8){
+  var j=0;
+  while(j<8){
+    if(i==4&&j==4){break;}
+    num++;
+    j++;
+  }
+  i++;  
+}
+console.log(num);
