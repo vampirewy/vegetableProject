cd2f38bda61a6392d862b40bdc836d8370ed3b20
cad2800 <haveaballmyball@hotmail.com>
2018-05-16 20:57:43 +0800

    no message

--- loop.js
+++ loop.js
@@ -3,7 +3,7 @@ var num=0;
 start:for (var i = 0 ; i < 8 ; i++){
   for (var j = 0 ; j < 8 ; j++){
     if( i == 4 && j == 4 ){
-      break start;
+      break start;//当使用label语句与break配合时,循环是整跳出,不再执行;
     }
     num++;  
   }
@@ -25,7 +25,7 @@ var num=0,i=0;
 while(i<8){
   var j=0;
   while(j<8){
-    if(i==4&&j==4){break;}
+    if(i==4&&j==4){break;}//使用for循环时,break只是当前满足条件时退出,但循环还是会继续,直到最后输出;
     num++;
     j++;
   }
