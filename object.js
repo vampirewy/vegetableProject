d7e7fa3d58221424cc86b2f1536a0c78c59361f5
冰凉的小肉包 <haveaballmyabll@hotmail.com>
2018-05-23 11:31:58 +0800

    增加如何减少创建全局变量文本

--- object.js
+++ object.js
@@ -1,18 +1,49 @@
-"use strict";
-function a(pageNum,pageSize,obj){
-  console.log(arguments);
-  console.log(pageNum,pageSize,obj); 
-};
-//这里可以放置你想要传进函数内的属性,灵活性比较大
-var object={
-  name:'wy',
-  age:'18'
-};
-//与形参一一对应
-a(1,20,object);
-//如果传的参数只有一个object,命名参数(形参)均为pageNum=obj,pageSize/obj=undefined
-a(object);
+// "use strict";
+// function a(pageNum,pageSize,obj){
+//   console.log(arguments);
+//   console.log(pageNum,pageSize,obj); 
+// };
+// //这里可以放置你想要传进函数内的属性,灵活性比较大
+// var object={
+//   name:'wy',
+//   age:'18'
+// };
+// //与形参一一对应
+// a(1,20,object);
+// //如果传的参数只有一个object,命名参数(形参)均为pageNum=obj,pageSize/obj=undefined
+// a(object);
 
-var arr=[0,1,5,10,15];
-arr.sort();
-console.log(arr);
+// var arr=[0,1,5,10,15];
+// arr.sort();
+// console.log(arr);
+function Person(name,age){
+  this.name=name;
+  this.age=age;
+  Person.prototype.run=function(){
+    console.log('跑步');
+  };
+  Person.prototype.eat=function(){
+    console.log('吃');
+  }
+}
+var person=new Person('小明','45');
+console.log(person);
+console.log(person.run());
+console.log(person.eat());
+
+var Person={
+  model:function(name,age){
+    this.name=name;
+    this.age=age;
+  }
+};
+Person.model.prototype.run=function(name){
+  console.log(`${name}跑步`);
+};
+Person.model.prototype.eat=function(name){
+  console.log(`${name}吃`)
+};
+var xiaoming=new Person.model('xiaoming','18');
+console.log(xiaoming.run(xiaoming.name));
+console.log(xiaoming.eat(xiaoming.name));
+
