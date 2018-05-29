4392370cbf3ed51fc837035ee334581ad3d4937f
cad2800 <haveaballmyball@hotmail.com>
2018-05-29 22:40:41 +0800

    no message

--- www/js/routers/router.js
+++ www/js/routers/router.js
@@ -3,4 +3,4 @@ module.exports=angular.module('starter.controllers',[
   require('../../modules/tab-chats/_service'),
   require('../../modules/tab-chats/chat-detail/_service'),
   require('../../modules/tab-account/_service')
-]).name
+])
