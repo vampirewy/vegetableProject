5d0a10bd7e5e1e7b556bb0720535c36a6dcd426b
cad2800 <haveaballmyball@hotmail.com>
2018-06-17 21:24:55 +0800

    no message

--- prototype.js
+++ prototype.js
@@ -1,35 +1,44 @@
 /**主要是关于原型与原型链的 */
 //构造函数
-// function Person() {
-//   console.log(111111);
-// };
-// Person.prototype.name = 'Mike'; //实例的原型对象
-// const boy = new Person(); //实例化
-// console.log(boy);
-// console.log(Person.prototype === boy.__proto__);
-
-// function Boy() {}; //构造函数
-// // var __prto = Object.create(Person.prototype); //复制一份父的原型副本
-// // __prto.constructor = Boy;
-// // Boy.prototype = __prto; //子类原型继承父类原型
-// // Boy.prototype.test = function () {}
-// // const he = new Boy();
-// // console.log(he);
-// // console.log(he.__proto__ === Boy.prototype);                                                                                                 
-// function __prto() {};
-// __prto.prototype = Person.prototype;
-// Boy.prototype = new __prto();
-// Boy.prototype.test = function () {};
-// const he = new Boy();
-// console.log(he);
-
-function Person(){};
-Person.prototype={
-  constructor:Person,
-  name:'Mike',
-  sayName:function(){
-    console.log(this.name);
-  }
+function Person() {
+  console.log(111111);
 };
-const friend=new Person();
-console.log(friend.sayName());
+Person.prototype.name = 'Mike'; //实例的原型对象
+const boy = new Person(); //实例化
+console.log(boy);
+console.log(Person.prototype === boy.__proto__);
+
+function Boy() {}; //构造函数
+var __prto = Object.create(Person.prototype); //复制一份父的原型副本
+__prto.name='hello';
+console.log(__prto.__proto__===Person.prototype);
+__prto.constructor = Boy;
+Boy.prototype = __prto; //子类原型继承父类原型
+Boy.prototype.test = function () {};
+Boy.prototype.name='Mary';
+const he = new Boy();
+console.log(he);
+console.log(he.__proto__ === Boy.prototype, Boy.prototype.__proto__ === Person.prototype, Person.prototype.__proto__ === Object.prototype);
+
+
+function F(word) {
+ //这边构造会被创建两次，第一次是S原型上，第二次是s实例上
+  this.word = word;
+};
+F.prototype.sayHello = function () {
+  return this.word
+};
+
+function S(word) {
+  F.call(this, word);
+};
+S.prototype = new F();
+S.prototype.constructor = S;
+S.prototype.sayBye = function () {
+  return this.words
+};
+S.prototype.bey = 'mike';
+var s = new S('hello');
+console.log(s);
+console.log(s.sayHello());
+console.log(s.bey);
