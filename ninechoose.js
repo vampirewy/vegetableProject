d3c825d218bf877449c0c7ec534064cfe062c967
wangyi01 <haveaballmyball@hotmail.com>
2018-06-19 18:01:20 +0800

    no message

---
+++ ninechoose.js
@@ -0,0 +1,360 @@
+var teamLists = [{
+    number: "1",
+    teamName: "英超",
+    time: "3:45开赛",
+    leftName: "阿森纳",
+    rightName: "曼城",
+    lists: [{
+        name: "3",
+        odds: "胜3.9",
+        active: true
+      },
+      {
+        name: "1",
+        odds: "平3.8",
+        active: true
+      },
+      {
+        name: "0",
+        odds: "负3.81",
+        active: true
+      }
+    ],
+    gameLists: [{
+        name: "3",
+        odds: "胜3.9",
+        active: true
+      },
+      {
+        name: "1",
+        odds: "平3.8",
+        active: true
+      },
+      {
+        name: "0",
+        odds: "负3.81",
+        active: true
+      }
+    ]
+  },
+  {
+    number: "2",
+    teamName: "英足总杯",
+    time: "3:45开赛",
+    leftName: "热刺",
+    rightName: "罗奇代",
+    lists: [{
+        name: "3",
+        odds: "胜3.9",
+        active: true
+      },
+      {
+        name: "1",
+        odds: "平8.64",
+        active: true
+      },
+      {
+        name: "0",
+        odds: "负23.9"
+      }
+    ],
+    gameLists: [{
+        name: "3",
+        odds: "胜3.9",
+        active: true
+      },
+      {
+        name: "1",
+        odds: "平8.64",
+        active: true
+      }
+    ]
+  },
+  {
+    number: "3",
+    teamName: "意杯",
+    time: "3:30开赛",
+    leftName: "尤文",
+    rightName: "亚特兰",
+    lists: [{
+        name: "3",
+        odds: "胜1.54",
+        active: true
+      },
+      {
+        name: "1",
+        odds: "平3.83"
+      },
+      {
+        name: "0",
+        odds: "负6.27"
+      }
+    ],
+    gameLists: [{
+      name: "3",
+      odds: "胜1.54",
+      active: true
+    }]
+  },
+  {
+    number: "4",
+    teamName: "意杯",
+    time: "3:45开赛",
+    leftName: "拉齐奥",
+    rightName: "AC米兰",
+    lists: [{
+        name: "3",
+        odds: "胜1.87"
+      },
+      {
+        name: "1",
+        odds: "平3.45"
+      },
+      {
+        name: "0",
+        odds: "负4.06"
+      }
+    ]
+    // gameLists: []
+  },
+  {
+    number: "5",
+    teamName: "意杯",
+    time: "3:45开赛",
+    leftName: "拉齐奥",
+    rightName: "AC米兰",
+    lists: [{
+        name: "3",
+        odds: "胜1.87",
+        active: true
+      },
+      {
+        name: "1",
+        odds: "平3.45"
+      },
+      {
+        name: "0",
+        odds: "负4.06",
+        active: true
+      }
+    ],
+    gameLists: [{
+        name: "3",
+        odds: "胜1.87",
+        active: true
+      },
+      {
+        name: "0",
+        odds: "负4.06",
+        active: true
+      }
+    ]
+  },
+  {
+    number: "6",
+    teamName: "意杯",
+    time: "3:45开赛",
+    leftName: "拉齐奥",
+    rightName: "AC米兰",
+    lists: [{
+        name: "3",
+        odds: "胜1.87"
+      },
+      {
+        name: "1",
+        odds: "平3.45"
+      },
+      {
+        name: "0",
+        odds: "负4.06",
+        active: true
+      }
+    ],
+    gameLists: [{
+        name: "0",
+        odds: "负4.06",
+        active: true
+      },
+      {
+        name: "1",
+        odds: "负4.06",
+        active: true
+      }
+    ]
+  }
+];
+
+function result(teamLists) {
+  var game = "",
+    a = "";
+  for (var p in teamLists) {
+    teamLists[p].gameNum = "";
+    if (teamLists[p].gameLists && teamLists[p].gameLists.length) {
+      for (var i in teamLists[p].gameLists) {
+        teamLists[p].gameLists[i].active &&
+          (teamLists[p].gameNum += teamLists[p].gameLists[i].name);
+      }
+    } else {
+      teamLists[p].gameNum = "~";
+    }
+    a = (game += teamLists[p].gameNum + ",").substring(0, game.length - 1);
+  }
+  return a;
+}
+var betResult = result(teamLists);
+
+var arr = [
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }, {
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }],
+  [{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  },{
+    name: "3",
+    odds: "胜3.9",
+    active: true
+  }]
+];
+
+function sortB(list) { 
+  console.log(list);
+  var start=new Date().getTime();
+  var r = [];
+  var l = [];
+  for (var i = 0; i < list.length; i++) {
+    var len = r.length;
+    for (var j = 0; j < list[i].length; j++) {
+      r.push([list[i][j]]);
+    };
+    for (var x = 0; x < len; x++) {
+      for (var y = 0; y < list[i].length; y++) {
+        r.push(r[x].concat([list[i][y]]));
+      };
+    };
+  };
+  for (var a = 0; a < r.length; a++) {
+    (r[a].length === 9) && l.push(r[a]);
+  };
+  console.log(l);
+  var end=new Date().getTime();
+  var over=end-start;
+  console.log(over/1000);
+  return l;
+};
+sortB(arr);
--- oneweek.js
+++ oneweek.js
@@ -1,10 +1,10 @@
 /*
-*上方这题主要涉及到的是变量提升、
-*局部作用域及全局作用域 
-* 1.函数提升比变量提升权重更重；函数声明式是整体提升,第一个alert(a)*实际是alert a函数;
-* 2.第二个alert(a)是弹出变量为a的函数，函数被重写了，当再启动时，值为1;但当var a不被赋值时，alert 2
-* 3.var c=d=a;从右向左赋值; d没用var声明为全局变量，c为函数局部变量,严格模式下d会报错;所以alert(c)是找不到的，会报错 
-*/
+ *上方这题主要涉及到的是变量提升、
+ *局部作用域及全局作用域 
+ * 1.函数提升比变量提升权重更重；函数声明式是整体提升,第一个alert(a)*实际是alert a函数;
+ * 2.第二个alert(a)是弹出变量为a的函数，函数被重写了，当再启动时，值为1;但当var a不被赋值时，alert 2
+ * 3.var c=d=a;从右向左赋值; d没用var声明为全局变量，c为函数局部变量,严格模式下d会报错;所以alert(c)是找不到的，会报错 
+ */
 // + function () {
 //   alert(a); //function a(){console.log(2)}
 //   a(); //2
@@ -44,225 +44,8 @@ var test = {
 };
 // var p = test.init(); //function go(){........}
 // p();
-new (test.init())(); //60  go函数为这个实例的构造函数
+new(test.init())(); //60  go函数为这个实例的构造函数
 
-var teamLists = [
-  {
-    number: "1",
-    teamName: "英超",
-    time: "3:45开赛",
-    leftName: "阿森纳",
-    rightName: "曼城",
-    lists: [
-      {
-        name: "3",
-        odds: "胜3.9",
-        active: true
-      },
-      {
-        name: "1",
-        odds: "平3.8",
-        active: true
-      },
-      {
-        name: "0",
-        odds: "负3.81",
-        active: true
-      }
-    ],
-    gameLists: [
-      {
-        name: "3",
-        odds: "胜3.9",
-        active: true
-      },
-      {
-        name: "1",
-        odds: "平3.8",
-        active: true
-      },
-      {
-        name: "0",
-        odds: "负3.81",
-        active: true
-      }
-    ]
-  },
-  {
-    number: "2",
-    teamName: "英足总杯",
-    time: "3:45开赛",
-    leftName: "热刺",
-    rightName: "罗奇代",
-    lists: [
-      {
-        name: "3",
-        odds: "胜3.9",
-        active: true
-      },
-      {
-        name: "1",
-        odds: "平8.64",
-        active: true
-      },
-      {
-        name: "0",
-        odds: "负23.9"
-      }
-    ],
-    gameLists: [
-      {
-        name: "3",
-        odds: "胜3.9",
-        active: true
-      },
-      {
-        name: "1",
-        odds: "平8.64",
-        active: true
-      }
-    ]
-  },
-  {
-    number: "3",
-    teamName: "意杯",
-    time: "3:30开赛",
-    leftName: "尤文",
-    rightName: "亚特兰",
-    lists: [
-      {
-        name: "3",
-        odds: "胜1.54",
-        active: true
-      },
-      {
-        name: "1",
-        odds: "平3.83"
-      },
-      {
-        name: "0",
-        odds: "负6.27"
-      }
-    ],
-    gameLists: [
-      {
-        name: "3",
-        odds: "胜1.54",
-        active: true
-      }
-    ]
-  },
-  {
-    number: "4",
-    teamName: "意杯",
-    time: "3:45开赛",
-    leftName: "拉齐奥",
-    rightName: "AC米兰",
-    lists: [
-      {
-        name: "3",
-        odds: "胜1.87"
-      },
-      {
-        name: "1",
-        odds: "平3.45"
-      },
-      {
-        name: "0",
-        odds: "负4.06"
-      }
-    ]
-    // gameLists: []
-  },
-  {
-    number: "5",
-    teamName: "意杯",
-    time: "3:45开赛",
-    leftName: "拉齐奥",
-    rightName: "AC米兰",
-    lists: [
-      {
-        name: "3",
-        odds: "胜1.87",
-        active: true
-      },
-      {
-        name: "1",
-        odds: "平3.45"
-      },
-      {
-        name: "0",
-        odds: "负4.06",
-        active: true
-      }
-    ],
-    gameLists: [
-      {
-        name: "3",
-        odds: "胜1.87",
-        active: true
-      },
-      {
-        name: "0",
-        odds: "负4.06",
-        active: true
-      }
-    ]
-  },
-  {
-    number: "6",
-    teamName: "意杯",
-    time: "3:45开赛",
-    leftName: "拉齐奥",
-    rightName: "AC米兰",
-    lists: [
-      {
-        name: "3",
-        odds: "胜1.87"
-      },
-      {
-        name: "1",
-        odds: "平3.45"
-      },
-      {
-        name: "0",
-        odds: "负4.06",
-        active: true
-      }
-    ],
-    gameLists: [
-      {
-        name: "0",
-        odds: "负4.06",
-        active: true
-      },
-      {
-        name: "1",
-        odds: "负4.06",
-        active: true
-      }
-    ]
-  }
-];
-function result(teamLists) {
-  var game = "",
-    a = "";
-  for (var p in teamLists) {
-    teamLists[p].gameNum = "";
-    if (teamLists[p].gameLists && teamLists[p].gameLists.length) {
-      for (var i in teamLists[p].gameLists) {
-        teamLists[p].gameLists[i].active &&
-          (teamLists[p].gameNum += teamLists[p].gameLists[i].name);
-      }
-    } else {
-      teamLists[p].gameNum = "~";
-    }
-    a = (game += teamLists[p].gameNum + ",").substring(0, game.length - 1);
-  }
-  return a;
-}
-var betResult = result(teamLists);
-// console.log(betResult);
 
 /**请在下面写出JavaScript面向对象编程的混合式继承。并写出ES6版本的继承。
 要求：汽车是父类，Cruze是子类。父类有颜色、价格属性，有售卖的方法。Cruze子
@@ -270,53 +53,51 @@ var betResult = result(teamLists);
 买给了小王价格是14万。*/
 
 /**父类 */
-function Car(color, name, money) {
+function Car(color, price) {
   this.color = color;
-  this.money = money;
-  this.name = name;
-}
-Car.prototype.sale = function() {
-  console.log(`${this.color}色的车卖给了${this.name}价格是${this.money}`);
+  this.price = price;
+};
+Car.prototype.sale = function () {
+  console.log(`${this.color}卖了${this.price}给${cruze.name}`);
 };
 /**子类 */
-function Cruze(color, name, money) {
-  // Car.call(this, color,name);
-  // Car.apply(this,arguments);
-  Car.apply(this, [color, name, money]);
-}
-//1.拿到父类原型链上的方法
-//2.不能让构造函数执行两次
-//3.引用的原型链不能是按址引用
-//4.修正子类的constructor
-var __pro = Object.create(Car.prototype);
-__pro.constructor = Cruze;
-Cruze.prototype = __pro;
-var cruze = new Cruze("red", "小王", "140000");
-console.log(cruze);
-console.log(cruze.sale());
+function Cruze(name, color, price) {
+  this.name = name;
+  Car.call(this, color, price);
+};
+var pro = Object.create(Car.prototype); //pro.__proto__ === Car.prototype 创建的是变量pro的原型
+Cruze.prototype = pro;
+Cruze.prototype.constructor = Cruze; //修正构造函数，可以使用属性
+var cruze = new Cruze('小王', '红色', 140000);
+cruze.sale();
 
-function Person(sex,height){
-  this.sex=sex;
-  this.height=height;
+//请用一句话算出0-100之间学生的学生等级，如90-100输出为1等生、80-90为2等生以此类推。不允许使用if switch等。
+function source(sources, level) {
+  var s = Math.round(level - sources / level) == 0 ? Math.round(level - sources / level) + 1 : Math.round(level - sources / level);
+  console.log(s);
 };
-Person.prototype.color=function(color){
-  return color;
+source(100, 10);
+
+//请用一句话遍历变量a。(禁止用for 已知var a = “abc”)
+var a = 'abc';
+var b = [...a];
+var c = Array.from(a);
+console.log(b, c);
+
+
+var s = [];
+var arr = s;
+/** s===arr */
+for (var i = 0; i < 3; i++) {
+  var pusher = {
+      value: "item" + i
+    },
+    tmp; //tmp未定义
+  if (i !== 2) {
+    tmp = [];
+    pusher.children = tmp;   //pusher = { children:[],value:'item'+i};
+  }
+  arr.push(pusher); // arr=s=[{children:[],value:'item0'}]
+  arr = tmp;  //arr === children:[];arr!=s 
 };
-Person.prototype.tall=function(){
-  return `这人${this.height}高`;
-}
-// var person=new Person('男','180');
-// console.log(person);
-// console.log(person.color('yellow'));
-// console.log(person.tall());
-function Boy(sex,height){
-  Person.call(this,sex,height);
-};
-var __proto=Object.create(Person.prototype); //创建原型副本,防止子类增加方法，父类也增加方法
-Boy.prototype=__proto; //原型指向副本，防止子类增加方法，父类也增加方法
-__proto.constructor=Boy; //修正构造函数
-Boy.prototype.test=function(){return 2222};
-var boy=new Boy('男','190');
-console.log(boy.tall());
-console.log(boy.sex);
-console.log(boy);
+console.log(s[0]);
