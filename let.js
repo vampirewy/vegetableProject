a36aa28ea9c452d17a7f8719176f07c01aea9c90
wangyi01 <haveaballmyball@hotmail.com>
2018-07-04 18:00:05 +0800

    no message

--- let.js
+++ let.js
@@ -31,19 +31,49 @@
 // };
 // let [first,second,third,four,five,six,seven]=fibs();
 // console.log(first,second,third,four,five,six,seven);
-let el = {
-  title: '1',
-  name: 'hello'
-};
-let {
-  title,
-  name
-} = el, gameLists = [];
-gameLists = {
-  title,
-  name
-};
-console.log(gameLists);
+// let el = {
+//   title: '1',
+//   name: 'hello'
+// };
+// let {
+//   title,
+//   name
+// } = el, gameLists = [];
+// gameLists = {
+//   title,
+//   name
+// };
+// console.log(gameLists);
 
-let {x,y=3}={x:1};
-console.log(y);
+// let {x,y=3}={x:1};
+// console.log(y);
+let arr = [{
+    name: '王',
+    size: '50'
+  }, //0
+  {
+    name: '李',
+    size: '65'
+  }, //1
+  {
+    name: '刘',
+    size: '70'
+  }, //2
+  {
+    name: '张',
+    size: '45'
+  }, //3
+];
+
+
+arr.sort(function (a, b) {
+  if (a.size < b.size) {
+    return 1
+  } else if (a.size > b.size) {
+    return -1
+  } else {
+    return 0
+  }
+});
+
+console.log(arr);
