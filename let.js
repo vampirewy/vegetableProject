b935ffad9251c75e71478cc12c2889a48e794767
wangyi01 <haveaballmyball@hotmail.com>
2018-07-05 10:03:28 +0800

    no message

--- let.js
+++ let.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 // var a = [];
 // for (let i = 0; i < 10; i++) {
 //   a[i] = function () {
@@ -47,84 +47,63 @@
 
 // let {x,y=3}={x:1};
 // console.log(y);
-let arr = [{
-    name: '10',
-    size: '50'
+let arr = [
+  {
+    name: "10",
+    size: "50"
   }, //0
   {
-    name: '李',
-    size: '65'
+    name: "李",
+    size: "65"
   }, //1
   {
-    name: '60',
-    size: '50'
+    name: "60",
+    size: "50"
   }, //2
   {
-    name: '张',
-    size: '50'
+    name: "张",
+    size: "50"
   }, //3
+  {
+    name: "王",
+    size: "60"
+  }, //4
+  {
+    name:'孙',
+    size:'80'
+  }//5
 ];
-let arr1 = [];
-let arr2 = [];
-// for (let i = 0; i < arr.length; i++) {
-//   if (i % 2 == 0) {
-//     // arr[i].infor = '';
-//     arr[i].infor = arr[i + 1];
-//     arr1.push(arr[i]);
-//   };
-// };
-// arr.map((el, index) => {
-//   if (index % 2 == 0) {
-//     arr[index].infor = arr[index + 1];
-//   };
-//   delete arr[index + 1];
-// });
-// arr1 = arr.filter((el, index) => {
-//   return el;
-// });
+start(arr);
 function start(arr) {
   if (arr instanceof Array) {
-    arr.map((el, index) => {
-      index % 2 == 0 && (arr[index].infor = arr[index + 1]);
+    let arr1 = [];
+    arr.forEach((el, index) => {
+      index % 2 == 0 && arr1.push(el);
     });
-    sort(arr);
+    sort(arr1);
   } else {
-    console.log(new Error('必须是数组'))
+    throw new Error("必须是数组");
   }
-};
-start(arr);
-
-function sort(arr) {
-  arr.sort((a, b) => {
-    if (a.size < b.size) {
-      return 1;
-    } else if (a.size > b.size) {
-      return -1;
+}
+function sort(item) {
+  let arr2 = [];
+  item.sort((a, b) => {
+    if (b.size == a.size) {
+      return b.name - a.name;
     } else {
-      if (a.name < b.name) {
-        return 1;
-      } else if (a.name > b.name) {
-        return -1;
-      } else {
-        return 0;
-      };
-    };
+      return b.size - a.size;
+    }
   });
-  reset(arr);
-};
-
-function reset(arr) {
-  let arr1 = [];
-  arr.forEach((el, index) => {
-    el.infor && arr1.push(el);
+  item.forEach((el1, index1) => {
+    arr.forEach((el, index) => {
+      el1.name == el.name && arr2.push(el1, arr[index + 1]);
+    });
   });
-  console.log(arr1);
-};
+  arr2=arr2.filter((el, index) => {
+    if (el) {
+      return el;
+    }
+  });
+  console.log(arr2);
+}
 
-// for (let end = 0; end < arr1.length; end++) {
-//   arr2.push(arr1[end], arr1[end].infor);
-//   arr2.forEach(value => {
-//     delete value.infor;
-//   });
-// };
-// console.log(arr2);
